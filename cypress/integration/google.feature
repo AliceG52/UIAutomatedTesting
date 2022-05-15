1ed67a7f8439617f19ebd0fb149bb5c03e067135
Alice G <alicegilmour52@hotmail.com>
2022-05-15 21:13:22 +0100

    Ran through the @badeball/cypress-cucumber-preprocessor quickstart and the webpack example.

---
+++ cypress/integration/google.feature
@@ -0,0 +1,7 @@
+Feature: Feature name
+
+    Feature Description
+
+Scenario: Scenario name
+When Start to type your When step here
+Then Start to type your Then step here
---
+++ cypress/integration/google.ts
@@ -0,0 +1,9 @@
+import { When, Then } from "@badeball/cypress-cucumber-preprocessor"
+
+When('Start to type your When step here', () => {
+    cy.visit('https://google.com')
+});
+
+Then('Start to type your Then step here', () => {
+
+});
